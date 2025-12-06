f0bc9648d42c4bcd0ca1f5ab26f458dbda772c2b
Shubham Chhatbar <shubhamchhatbar1@gmail.com>
2025-12-06 15:42:53 +0530

    just practicing

--- hexdump.c
+++ hexdump.c
@@ -1,7 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+
+void print_clear(char *s, int len){
+    printf("|");
+    for(int i=0; i<len; i++){
+        if(s[i] < 0x20 || s[i] > 0x7E){
+            //replace special char with .
+            printf(".");
+        }else{
+            //print actual char
+            printf("%c", s[i]); 
+        }
+    }
+    printf("|"); 
+}
 
 int main(){
-    
-}
+    int c;
+    int mi = 0;
+    char *buffer = (char *)calloc(0x10, sizeof(char));
+    while((c=getchar()) != EOF){
+        buffer[mi % 0x10] = c;
+        if(mi % 16 == 0x0) printf("%08x ", mi); 
+        printf("%02x ", c);
+        mi++;
+
+        if(mi % 16 == 0x0){
+            //last memory add in row
+            print_clear(buffer, 0x10);
+            printf("\n");
+        }
+    }
+    int last_mi = mi%16;
+    printf("%*s", 0x10*3-last_mi*3, " ");
+    print_clear(buffer, last_mi);
+    printf("\n%08x", mi);
+    printf("\n");
+} 
